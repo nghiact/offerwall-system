345fda1e9e57b42551b709cef42e0185871b3394
CTNLovesGaming <caotuannghia2208@gmail.com>
2026-05-27 22:25:27 +0700

    fix(gateway): expose offer categories

--- api-gateway/src/test/java/com/ctn/offerwall/api/ApiGatewayApplicationTests.java
+++ api-gateway/src/test/java/com/ctn/offerwall/api/ApiGatewayApplicationTests.java
@@ -1,13 +1,28 @@
 package com.ctn.offerwall.api;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class ApiGatewayApplicationTests {
+
+    @Autowired
+    private RouteDefinitionLocator routeDefinitionLocator;
 
     @Test
     void contextLoads() {
     }
 
+    @Test
+    void exposesOfferCategoryRoute() {
+        assertThat(routeDefinitionLocator.getRouteDefinitions()
+                .map(routeDefinition -> routeDefinition.getId())
+                .collectList()
+                .block())
+                .contains("offer-category-service");
+    }
 }
